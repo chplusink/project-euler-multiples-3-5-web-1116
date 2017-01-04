5f8b33b3b0953f11205191fa8e1546836d2cae78
Vic Lam <lam.vic.k@gmail.com>
2017-01-04 09:30:59 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,8 @@
 # Enter your procedural solution here!
+def collect_multiples(num)
+  (1..(num-1)).to_a.select { |i| i % 3 == 0 || i % 5 == 0 }
+end
+
+def sum_multiples(num)
+  collect_multiples(num).inject(0) { |sum,i| sum+i }
+end
